53746d1c69793d7e8c31e2d1db7423fa1178b607
taiki yamashita <taikity0808@gmail.com>
2021-11-13 11:31:23 +0900

    feature: クエリパラメータの型変換

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 from fastapi import FastAPI
 # FastAPIは、APIのすべての機能を提供するPythonクラス
@@ -48,3 +49,16 @@ async def get_model(model_name: ModelName):
     if model_name == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
     return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+    

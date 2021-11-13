ad442650695badc1c95c9631fbaad92ad1feabaf
taiki yamashita <taikity0808@gmail.com>
2021-11-13 11:36:03 +0900

    複数のパスパラメータとクエリパラメータを定義

--- main.py
+++ main.py
@@ -40,6 +40,19 @@ async def read_user_me():
 async def read_user(user_id: str):
     return {"/user_id": user_id}
 
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that a long description"}
+        )
+    return item
+
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     # ModelNameクラスではstr型を定義しているので値は文字列にならないといけない。
@@ -61,4 +74,3 @@ async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
         )
     return item
 
-    
